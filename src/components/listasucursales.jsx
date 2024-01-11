a34b6a6b21db022a6b7a33a1c9baf2d1bf53437c
Jose Alvarez <jalvarezm7@uteq.edu.ec>
2024-01-11 02:06:21 -0500

    Correcion crear sucursal

--- src/components/listasucursales.jsx
+++ src/components/listasucursales.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Space, Button, Table, Modal, message, Switch, Form, Input } from 'antd';
 import mapa from './res/mapa.png';
 import AdminSucursal from './adminsucursal';
-import CrearSucursal from './CrearSucursal';
+import Crearsucursal from './crearsucursal';
 
 const ListSucursales = () => {
     const [loading, setLoading] = useState(true);
@@ -190,7 +190,7 @@ const ListSucursales = () => {
                 onCancel={handleCancel}
                 footer={null}
             >
-                <CrearSucursal />
+                <Crearsucursal />
             </Modal>
         </div>
     );
